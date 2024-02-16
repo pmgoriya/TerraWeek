c3b3cf6e4f0c437296fe0a0253f55bab03b9d875
Pankaj <pmgoriya@gmail.com>
2024-02-16 11:38:17 +0530

    Day04 don

--- day04/provider.tf
+++ day04/provider.tf
@@ -5,6 +5,12 @@ terraform {
 			version = "~>5.0"
 		}
 	}
+	backend "s3" {
+    bucket         = "pmgoriya-qr"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
